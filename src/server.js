a17193ebd40480f71cb65bc9de4a8408c159f26d
Johanne <johanne_lewis@hotmail.com>
2019-08-22 20:43:47 +0100

    Read html file, and display contents in browser

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const path = require('path');
+
 const app = new express();
 
 app.get('/', function(req, res){
-    res.send('Hello world it is Johanne');
+    const pathToHtmlFile = path.resolve(__dirname, '../dist/index.html');
+    const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
+    res.send(contentFromHtmlFile);
 });
 
 app.listen(3000, function(){
